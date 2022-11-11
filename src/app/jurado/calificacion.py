7c7452cfe2e4211f19dbde32e1bcb992f8ac75df
Leonardo Ramírez <31749560+LeoRami99@users.noreply.github.com>
2022-11-11 00:59:18 -0500

    Calificación arreglada, falta que ya no se vea en la vista

--- src/app/jurado/calificacion.py
+++ src/app/jurado/calificacion.py
@@ -2,26 +2,43 @@ from config import *
 conn = EstablecerConexion()
 cursor = conn.cursor()
 class Calificacion():
-    def __init__(self, calificacion, observaciones, fecha, id_proyecto, id_jurado, nota_jurado, nota_jurado_2, nota_jurado_3, nota_jurado_4, nota_jurado_5):
-        self.calificacion = calificacion
-        self.observaciones = observaciones
-        self.fecha = fecha 
+    def __init__(self,id_proyecto, id_jurado, nota_jurado1, nota_jurado_2, nota_jurado_3, nota_jurado_4, nota_jurado_5,  observaciones1, observaciones_2, observaciones_3, observaciones_4, observaciones_5): 
         self.id_proyecto = id_proyecto
         self.id_jurado = id_jurado
-        self.nota_jurado = nota_jurado
+        self.nota_jurado1 = nota_jurado1
         self.nota_jurado_2 = nota_jurado_2
         self.nota_jurado_3 = nota_jurado_3
         self.nota_jurado_4 = nota_jurado_4
         self.nota_jurado_5 = nota_jurado_5
+        self.observaciones1 = observaciones1
+        self.observaciones_2 = observaciones_2
+        self.observaciones_3 = observaciones_3
+        self.observaciones_4 = observaciones_4
+        self.observaciones_5 = observaciones_5
+
+
         
     def set_calificacion_observacion_jurado_uno(self):
-        try:
-            cursor.execute("UPDATE calificacion SET calificacion = %s, observaciones = %s, nota_jurado = %s WHERE id_proyecto = %s AND id_jurado = %s", (self.calificacion, self.observaciones, self.nota_jurado, self.id_proyecto, self.id_jurado))
-            conn.commit()
-            return True
-        except:
-            return False
-        
+        sql_califacion_uno = "UPDATE projecto_calificaciones SET cali_jurado_uno = %s, obser_jurado_uno = %s WHERE id_proyecto_id = %s"
+        cursor.execute(sql_califacion_uno, (self.nota_jurado1, self.observaciones1, self.id_proyecto))
+        conn.commit()
+    def set_calificacion_observacion_jurado_dos(self):
+        sql_califacion_dos = "UPDATE projecto_calificaciones SET cali_jurado_dos = %s, obser_jurado_dos = %s WHERE id_proyecto_id = %s"
+        cursor.execute(sql_califacion_dos, (self.nota_jurado_2, self.observaciones_2, self.id_proyecto))
+        conn.commit()
+    def set_calificacion_observacion_jurado_tres(self):
+        sql_califacion_tres = "UPDATE projecto_calificaciones SET cali_jurado_tres = %s, obser_jurado_tres = %s WHERE id_proyecto_id = %s"
+        cursor.execute(sql_califacion_tres, (self.nota_jurado_3, self.observaciones_3, self.id_proyecto))
+        conn.commit()
+    def set_calificacion_observacion_jurado_cuatro(self):
+        sql_califacion_cuatro = "UPDATE projecto_calificaciones SET cali_jurado_cuatro = %s, obser_jurado_cuatro = %s WHERE id_proyecto_id = %s"
+        cursor.execute(sql_califacion_cuatro, (self.nota_jurado_4, self.observaciones_4, self.id_proyecto))
+        conn.commit()
+    def set_calificacion_observacion_jurado_cinco(self):
+        sql_califacion_cinco = "UPDATE projecto_calificaciones SET cali_jurado_cinco = %s, obser_jurado_cinco = %s WHERE id_proyecto_id = %s"
+        cursor.execute(sql_califacion_cinco, (self.nota_jurado_5, self.observaciones_5, self.id_proyecto))
+        conn.commit()
+
         
 def get_califaciones(id_proyecto):
     sql = "SELECT * FROM projecto_calificaciones WHERE id_proyecto_id = {}".format(id_proyecto)
--- src/app/jurado/jurado.py
+++ src/app/jurado/jurado.py
@@ -39,12 +39,13 @@ def get_usuario_nombre(id_user):
 @jurado.route('/retroalimentacion', methods=['POST'])
 def retroalimentacion():
     if request.method == 'POST':
-        calificacion = request.form['calificacion']
-        observaciones = request.form['observaciones']
-        fecha_hora = datetime.now() 
         id_proyecto = request.form['id_proyecto']
-        id_jurado = request.form['id_usuario_jurado']
+        calificacion = request.form['calificacion']
+        id_jurado = request.form['id_jurado']
+        observaciones = request.form['observaciones']
+        ciclo = request.form['ciclo']
         data_calificacion=w3.toHex(text=calificacion)
+        
         data_observaciones=w3.toHex(text=observaciones)
         tx_calificacion={
             'from': "0xbB98661629e0d9263F527acB44084BFDB0d9b86c",
@@ -66,9 +67,38 @@ def retroalimentacion():
         # Se obtiene el hash de la transacción para cada documento
         hash_trans_califacion = tx_send_trans_califacion.hex()
         hash_trans_observacion = tx_send_trans_observacion.hex()
-        calificacion = Calificacion(hash_trans_califacion, hash_trans_observacion, fecha_hora, id_proyecto, id_jurado)
-        calificacion.set_calificaciones()
+        sql_proyecto="SELECT jurados_ciclo_id FROM projecto_proyecto WHERE id_proyecto = {}".format(int(id_proyecto))
+        cursor.execute(sql_proyecto)
+        jurados_ciclo_id = cursor.fetchone()
+        sql_grupo_jurado="SELECT * FROM projecto_juradosciclo WHERE ciclo =%s and id_jurado_ciclo = %s"
+        cursor.execute(sql_grupo_jurado, (ciclo, jurados_ciclo_id))
+        grupo_jurado = cursor.fetchall()
+        
+        for jurado_casilla in grupo_jurado:
+            print(jurado_casilla)
+            if jurado_casilla[4]==int(id_jurado):
+                calif_jurado_uno=Calificacion(id_proyecto,id_jurado,hash_trans_califacion, None, None, None, None, hash_trans_observacion, None, None, None, None)
+                calif_jurado_uno.set_calificacion_observacion_jurado_uno()
+            elif jurado_casilla[2]==int(id_jurado):
+                calif_jurado_dos=Calificacion(id_proyecto,id_jurado,None,hash_trans_califacion, None, None, None, None, hash_trans_observacion, None, None, None)
+                calif_jurado_dos.set_calificacion_observacion_jurado_dos()
+            elif jurado_casilla[3]==int(id_jurado):
+
+                calif_jurado_tres=Calificacion(id_proyecto,id_jurado,None,None, hash_trans_califacion, None, None, None, None, hash_trans_observacion, None, None)
+                calif_jurado_tres.set_calificacion_observacion_jurado_tres()
+            elif jurado_casilla[5]==int(id_jurado):
+
+                calif_jurado_cuatro=Calificacion(id_proyecto,id_jurado,None,None, None,hash_trans_califacion,None, None, None, None, hash_trans_observacion, None)
+                calif_jurado_cuatro.set_calificacion_observacion_jurado_cuatro()
+            elif jurado_casilla[6]==int(id_jurado):
+
+                calif_jurado_cinco=Calificacion(id_proyecto,id_jurado,None,None, None,None,hash_trans_califacion, None, None, None, None, hash_trans_observacion)
+                calif_jurado_cinco.set_calificacion_observacion_jurado_cinco()
         return redirect(url_for('jurado.perfiljurado'))
+
+    
+
+        
 
 
 
